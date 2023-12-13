898710df3285c6b7ab3f0963411672edf5beabf7
robotics4 <153777117+robotics4@users.noreply.github.com>
2023-12-13 14:02:42 -0800

    Update move-motor.py

--- move-motor.py
+++ move-motor.py
@@ -4,6 +4,6 @@ import runloop
 
 async def main():
     # Run a motor on port A for 360 degrees at 720 degrees per second.
-    await motor.run_for_degrees(port.A, 360, 720)
+    await motor.run_for_degrees(port.A, 360, 360)
     
 runloop.run(main())
