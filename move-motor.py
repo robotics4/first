e24d65adc0ad341d2ee0e804a1d7680157708fc6
robotics4 <153777117+robotics4@users.noreply.github.com>
2023-12-13 14:01:58 -0800

    Create move-motor.py

---
+++ move-motor.py
@@ -0,0 +1,9 @@
+import motor
+from hub import port
+import runloop
+
+async def main():
+    # Run a motor on port A for 360 degrees at 720 degrees per second.
+    await motor.run_for_degrees(port.A, 360, 720)
+    
+runloop.run(main())
