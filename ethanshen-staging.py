382ddf18b650258a68edc9c9e5a8ca1ffe0ac075
EthanYShen <153787534+EthanYShen@users.noreply.github.com>
2023-12-13 19:42:30 -0800

    Update ethanshen-staging.py

--- ethanshen-staging.py
+++ ethanshen-staging.py
@@ -1,1 +1,21 @@
+import motor
+import time
+
+from hub import port
+motor.run_to_absolute_position(port.E, 17, 360)
+time.sleep(0.1)
+motor.run_for_degrees(port.D, 200, 200)
+motor.run_for_degrees(port.F, -200, 200)
+time.sleep(1)
+motor.run_for_degrees(port.D, 200, 200)
+motor.run_for_degrees(port.F, -200, 200)
+time.sleep(1)
+motor.run_for_degrees(port.E, 40, 200)
+time.sleep(0.2)
+motor.run_for_degrees(port.D, -400, 200)
+motor.run_for_degrees(port.F, 400, 200)
+time.sleep(1)
+motor.run_for_degrees(port.E, 140, 200)
+time.sleep(1)
+motor.run_for_degrees(port.E, -180, 360)
 
