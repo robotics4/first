406a1300c097c26b212ad6336afc8ed0a3492adf
penguinboi2023 <153787407+penguinboi2023@users.noreply.github.com>
2023-12-20 19:50:52 -0800

    I did code
    I made the robot move. WOW.

--- jakob-staging.py
+++ jakob-staging.py
@@ -1,1 +1,8 @@
-
+#importing too much stuff
+from hub import port
+import time
+import motor
+#sets the position for arm
+motor.run_to_absolute_position(port.E,10,200)
+motor.run_for_degrees(port.D,-350,200)
+motor.run_for_degrees(port.F,350,200)
