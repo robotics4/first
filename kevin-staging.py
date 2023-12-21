d6bbaac024991b52bc9c6558551ecab4433dd4d3
Legosteve20 <153787330+Legosteve20@users.noreply.github.com>
2023-12-20 19:50:16 -0800

    Update kevin-staging.py

--- kevin-staging.py
+++ kevin-staging.py
@@ -8,7 +8,7 @@ motor.run_for_degrees(port.D, -400, 100)
 motor.run_for_degrees(port.F, 400, 100)
 # makes robot move foward
 time.sleep(1.5)
-motor.run_to_absolute_position(port.E, 20, 100)
+motor.run_for_degrees(port.E, 40, 50)
 time.sleep(1.5)
 motor.run_for_degrees(port.D, 400, 100)
 motor.run_for_degrees(port.F, -400, 100)
