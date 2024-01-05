36f544088e264f6e25f8046476f9261e09b7fd97
penguinboi2023 <153787407+penguinboi2023@users.noreply.github.com>
2024-01-05 10:48:46 -0800

    Code update
    I changed the code by finishing it, and it works at least 50% of the time.

--- jakob-staging.py
+++ jakob-staging.py
@@ -3,6 +3,19 @@ from hub import port
 import time
 import motor
 #sets the position for arm
-motor.run_to_absolute_position(port.E,10,200)
-motor.run_for_degrees(port.D,-350,200)
-motor.run_for_degrees(port.F,350,200)
+motor.run_to_absolute_position(port.E,40,200)
+time.sleep(0.3)
+motor.run_to_absolute_position(port.E,13,60)
+time.sleep(0.9)
+##insert movement towards energy here
+#aproaches energy
+motor.run_for_degrees(port.D,-400,200)
+motor.run_for_degrees(port.F,400,200)
+time.sleep(0.9)
+#prepares for transport
+motor.run_to_absolute_position(port.E,30,200)
+time.sleep(0.6)
+##insert movement to mission here
+#turn (wait wat)
+#DUNK
+motor.run_to_absolute_position(port.E,180,80)
