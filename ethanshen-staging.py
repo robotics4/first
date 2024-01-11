125a220f990526fdd0a3d74d53eb7d012769d232
EthanYShen <153787534+EthanYShen@users.noreply.github.com>
2024-01-10 19:27:36 -0800

    Update ethanshen-staging.py
    Now it is civilized

--- ethanshen-staging.py
+++ ethanshen-staging.py
@@ -7,8 +7,8 @@ motor.run_to_absolute_position(port.E, 10, 360)
 #gets the thrower ready
 time.sleep(0.05)
 #waits for the amount of time it takes to load the thrower.
-motor.run_for_degrees(port.D, -450, 200)
-motor.run_for_degrees(port.F, 450, 200)
+motor.run_for_degrees(port.D, -250, 200)
+motor.run_for_degrees(port.F, 250, 200)
 #move forward to grab the energy
 time.sleep(1)
 #waits the amount of time it takes to get to the energy
@@ -16,12 +16,12 @@ motor.run_for_degrees(port.E, 30, 50)
 #raises the thrower a little bit higher so that gravity will pull it down and then the energy won't fall down
 time.sleep(0.9)
 #waits the amount of time it takes to move the thrower a bit higher
-motor.run_for_degrees(port.D, 300, 200)
-motor.run_for_degrees(port.F, -300, 200)
+motor.run_for_degrees(port.D, 220, 200)
+motor.run_for_degrees(port.F, -220, 200)
 #moves the motors backwards towards the toy factory
 time.sleep(1.5)
 #waits the amount of time it takes to move backwards
-motor.run_for_degrees(port.E, 180, 300)
+motor.run_for_degrees(port.E, 220, 250)
 #throws the energy
 time.sleep(0.7)
 #waits the amount of time it takes to throw the energy
