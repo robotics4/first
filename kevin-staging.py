694aaad9b2ca9bca7f5e04081a6841316682dfb6
Legosteve20 <153787330+Legosteve20@users.noreply.github.com>
2024-01-31 19:00:12 -0800

    Update kevin-staging.py

--- kevin-staging.py
+++ kevin-staging.py
@@ -2,7 +2,7 @@ import motor
 import time
 from hub import port
 
-motor.run_to_absolute_position(port.E, 14, 150)
+motor.run_to_absolute_position(port.E, 8, 150)
 # makes the motor hand go to perfect position
 motor.run_for_degrees(port.D, -400, 100)
 motor.run_for_degrees(port.F, 400, 100)
