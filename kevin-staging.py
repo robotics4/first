6d0b144d93a6c9c63118bbcc90d36c48b235e980
Legosteve20 <153787330+Legosteve20@users.noreply.github.com>
2024-01-31 19:44:37 -0800

    Update kevin-staging.py

--- kevin-staging.py
+++ kevin-staging.py
@@ -3,7 +3,20 @@ import time
 import distance_sensor
 from hub import port
 
-
+while not color_sensor.color(port.A) is color.RED:
+    if color_sensor.reflection(port.A) > 70:
+        motor.run_for_degrees(port.D, -3, 100)
+    else:
+        motor.run_for_degrees(port.F, 3, 100)
+time.sleep(3.4)
+motor.run_for_degrees(port.D, -380, 200)
+time.sleep(2.2)
+while not color_sensor.color(port.A) is color.BLUE:
+    if color_sensor.reflection(port.A) > 70:
+        motor.run_for_degrees(port.D, -3, 100)
+    else:
+        motor.run_for_degrees(port.F, 3, 100)
+time.sleep(2.5)
 motor.run_to_absolute_position(port.E, 8, 150)
 # makes the motor hand go to perfect position
 motor.run_for_degrees(port.D, -400, 100)
