315029faf85ca43631427907e9059201c13c30ff
penguinboi2023 <153787407+penguinboi2023@users.noreply.github.com>
2024-01-31 18:36:14 -0800

    Update distancesensor.py
    I added my code

--- distancesensor.py
+++ distancesensor.py
@@ -1,6 +1,23 @@
-import hub
+from hub import light_matrix
+import motor
+from hub import port
+import runloop
 import time
+import hub
 import distance_sensor
-while True:
-    if distance(B, 10):
-        
+
+if distance_sensor.distance(9):
+    ##insert movement to mission here
+    #move backwards/approach mission
+    motor.run_for_degrees(port.D,360,200)
+    motor.run_for_degrees(port.F,-360,200)
+    #turn (wait wat)
+    #DUNK
+    motor.run_to_absolute_position(port.E,180,100)
+    time.sleep(2)
+    motor.run_to_absolute_position(port.E,10,100)
+    light_matrix.write("WE COOL")
+
+
+
+
