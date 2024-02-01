d2ae556c1f8c6b4a99ec44e8ac5c3ee2f51aeb15
JaydenPGAgolfer2013 <153787556+JaydenPGAgolfer2013@users.noreply.github.com>
2024-01-31 19:53:21 -0800

    Update jayden-staging.py

--- jayden-staging.py
+++ jayden-staging.py
@@ -15,15 +15,22 @@ while not color_sensor.color(port.A) is color.RED:
     else:
         motor.run_for_degrees(port.F, 3, 100)
 time.sleep(4)
-motor.run_for_degrees(port.D, -320, 200)
+motor.run_for_degrees(port.D, -300, 200)
 time.sleep(2.2)
 while not color_sensor.color(port.A) is color.BLUE:
     if color_sensor.reflection(port.A) > 70:
         motor.run_for_degrees(port.D, -3, 200)
     else:
         motor.run_for_degrees(port.F, 3, 200)
+while not color_sensor.color(port.A) is color.BLACK:
+    if color_sensor.reflection(port.A) > 70:
+        motor.run_for_degrees(port.D, -3, 200)
+    else:
+        motor.run_for_degrees(port.F, 3, 200)
 time.sleep(4)
-motor.run_for_degrees(port.D, -640, 200)
+while distance_sensor.distance(port. C)>50:
+    motor.run_for_degrees(port.D, 1, 200)
+motor.run_for_degrees(port.D, -600, 200)
 motor.run_to_absolute_position(port.E, 8, 120)
 time.sleep(5)
 motor.run_for_degrees(port.D, -200, 200)
@@ -31,8 +38,5 @@ motor.run_for_degrees(port.F, 200, 200)
 time.sleep(1)
 motor.run_for_degrees(port.E, 30, 180)
 time.sleep(1)
-while distance_sensor.distance(port. C)>50:
-    motor.run_for_degrees(port.D, 10, 200)
-    motor.run_for_degrees(port.F, -10, 200)
 time.sleep(1)
 motor.run_for_degrees(port.E, 400, 180)
