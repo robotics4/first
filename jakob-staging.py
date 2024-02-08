ed04c443043e1e9ba320cc7e8dcca4b75242ac18
penguinboi2023 <153787407+penguinboi2023@users.noreply.github.com>
2024-02-07 19:44:45 -0800

    Update jakob-staging.py
    I added all of the movement codes.

--- jakob-staging.py
+++ jakob-staging.py
@@ -2,12 +2,16 @@
 from hub import port
 import time
 import motor
+from hub import light_matrix
+import color_sensor
+import color
 #sets the position for arm
 motor.run_to_absolute_position(port.E,40,200)
-time.sleep(0.3)
-motor.run_to_absolute_position(port.E,13,60)
-time.sleep(0.9)
+time.sleep(1)
+motor.run_to_absolute_position(port.E,12,100)
+time.sleep(1.8)
 ##insert movement towards energy here
+
 #aproaches energy
 motor.run_for_degrees(port.D,-400,200)
 motor.run_for_degrees(port.F,400,200)
@@ -16,6 +20,31 @@ time.sleep(0.9)
 motor.run_to_absolute_position(port.E,30,200)
 time.sleep(0.6)
 ##insert movement to mission here
+while not color_sensor.color(port.A) is color.RED:
+    if color_sensor.color(port.A) is color.BLACK:
+        motor.run_for_degrees(port.D,-1,200)
+        time.sleep(0.001)
+    else:
+        motor.run_for_degrees(port.F,1,200)
+        time.sleep(0.001)
+    if color_sensor.color(port.A) is color.RED:
+        motor.run_for_degrees(port.D,200,200)
+while not color_sensor.color(port.A) is color.BLUE:
+    if color_sensor.color(port.A) is color.BLACK:
+        motor.run_for_degrees(port.D,1,200)
+        time.sleep(0.001)      
+    else:
+        motor.run_for_degrees(port.F,-1,200)
+        time.sleep(0.001)
+    if color_sensor.color(port.A) is color.BLUE:
+        motor.run_for_degrees(port.D,200,200)
+        motor.run_for_degrees(port.F,200,200)
+#move backwards/approach mission
+motor.run_for_degrees(port.D,360,200)
+motor.run_for_degrees(port.F,-360,200)
 #turn (wait wat)
 #DUNK
-motor.run_to_absolute_position(port.E,180,80)
+motor.run_to_absolute_position(port.E,180,100)
+time.sleep(2)
+motor.run_to_absolute_position(port.E,12,100)
+light_matrix.write("WE COOL")
