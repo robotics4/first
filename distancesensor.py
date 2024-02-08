5bb720f36cf7d7231eae8315134ceb78d9b4b7f6
penguinboi2023 <153787407+penguinboi2023@users.noreply.github.com>
2024-02-07 19:37:50 -0800

    Update distancesensor.py

--- distancesensor.py
+++ distancesensor.py
@@ -7,13 +7,7 @@ import hub
 import distance_sensor
 
 if distance_sensor.distance(20):
-    ##insert movement to mission here
-    #turn (wait wat)
-    #DUNK
-    motor.run_to_absolute_position(port.E,180,100)
-    time.sleep(2)
-    motor.run_to_absolute_position(port.E,10,100)
-    light_matrix.write("WE COOL")
+    # insert stuff to do next here
 
 
 
