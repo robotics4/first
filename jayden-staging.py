ad376020f41eb77026c7561407d8495c74b49a7f
JaydenPGAgolfer2013 <153787556+JaydenPGAgolfer2013@users.noreply.github.com>
2024-02-14 19:44:33 -0800

    Update jayden-staging.py
    yes

--- jayden-staging.py
+++ jayden-staging.py
@@ -40,3 +40,36 @@ motor.run_for_degrees(port.E, 30, 180)
 time.sleep(1)
 time.sleep(1)
 motor.run_for_degrees(port.E, 400, 180)
+#find the energy
+import motor
+import time
+import distance_sensor
+from hub import port
+import motor
+import time
+t=0
+motor.run_to_absolute_position(port.E, 10, 100)
+motor.run_for_degrees(port.D, 100, 100)
+motor.run_for_degrees(port.F, 100, 100)
+time.sleep(0.5)
+motor.run_for_degrees(port.E, 100, 100)
+motor.run_to_absolute_position(port.E, 10, 100)
+for i in range(0, 50):
+    if distance_sensor.distance(port.B)<50:
+            t=1
+    if t == 1:
+        break
+    motor.run_for_degrees(port.D, 1, 200)
+    time.sleep(1)
+        
+    
+motor.run_for_degrees(port.D, -600, 200)
+motor.run_to_absolute_position(port.E, 10, 120)
+time.sleep(5)
+motor.run_for_degrees(port.D, -200, 200)
+motor.run_for_degrees(port.F, 200, 200)
+time.sleep(1)
+motor.run_for_degrees(port.E, 30, 180)
+time.sleep(1)
+time.sleep(1)
+motor.run_for_degrees(port.E, 400, 180)
