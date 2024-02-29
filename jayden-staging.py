e596fb69035b059e39c2f91682a37b9542a1d499
JaydenPGAgolfer2013 <153787556+JaydenPGAgolfer2013@users.noreply.github.com>
2024-02-28 19:49:02 -0800

    Update jayden-staging.py
    2/28/2024

--- jayden-staging.py
+++ jayden-staging.py
@@ -9,29 +9,38 @@ import color
 #imports stuff
 from hub import port
 #imports the ports
+motor.run_to_absolute_position(port.E, 10, 120)
 while not color_sensor.color(port.A) is color.RED:
     if color_sensor.reflection(port.A) > 70:
         motor.run_for_degrees(port.D, -3, 100)
     else:
         motor.run_for_degrees(port.F, 3, 100)
-time.sleep(4)
+time.sleep(0.3)
 motor.run_for_degrees(port.D, -300, 200)
 time.sleep(2.2)
 while not color_sensor.color(port.A) is color.BLUE:
     if color_sensor.reflection(port.A) > 70:
-        motor.run_for_degrees(port.D, -3, 200)
+        motor.run_for_degrees(port.D, -1, 200)
     else:
-        motor.run_for_degrees(port.F, 3, 200)
-while not color_sensor.color(port.A) is color.BLACK:
-    if color_sensor.reflection(port.A) > 70:
-        motor.run_for_degrees(port.D, -3, 200)
-    else:
-        motor.run_for_degrees(port.F, 3, 200)
-time.sleep(4)
-while distance_sensor.distance(port. C)>50:
-    motor.run_for_degrees(port.D, 1, 200)
-motor.run_for_degrees(port.D, -600, 200)
+        motor.run_for_degrees(port.F, 1, 200)
 motor.run_to_absolute_position(port.E, 10, 120)
+time.sleep(0.4)
+for i in range(0, 50):
+    motor.run_for_degrees(port.D, -3, 200)
+    motor.run_for_degrees(port.F, 3, 200)
+motor.run_for_degrees(port.E, 20, 200)
+#put it on the blue marker so it is easily more accurate and could pick the energy up
+#motor.run_for_degrees(port.F, 100, 200)
+#while distance_sensor.distance(port. C)>50:
+#    motor.run_for_degrees(port.D, 1, 200)
+#motor.run_for_degrees(port.D, -600, 200)
+#motor.run_to_absolute_position(port.E, 10, 120)
+
+
+
+
+
+"""
 time.sleep(5)
 motor.run_for_degrees(port.D, -200, 200)
 motor.run_for_degrees(port.F, 200, 200)
@@ -61,8 +70,8 @@ for i in range(0, 50):
         break
     motor.run_for_degrees(port.D, 1, 200)
     time.sleep(1)
-        
-    
+
+
 motor.run_for_degrees(port.D, -600, 200)
 motor.run_to_absolute_position(port.E, 10, 120)
 time.sleep(5)
@@ -73,3 +82,4 @@ motor.run_for_degrees(port.E, 30, 180)
 time.sleep(1)
 time.sleep(1)
 motor.run_for_degrees(port.E, 400, 180)
+"""
