f6105ab34ae3a090c9b30982bf416c58e51e4022
EthanYShen <153787534+EthanYShen@users.noreply.github.com>
2024-03-06 19:09:55 -0800

    Update ethanshen-staging.py
    Distance sensor.

--- ethanshen-staging.py
+++ ethanshen-staging.py
@@ -1,4 +1,5 @@
 import motor
+import distance_sensor
 import time
 import color_sensor
 import color
@@ -7,9 +8,9 @@ from hub import port
 #imports the ports
 while not color_sensor.color(port.A) is color.RED:
     if color_sensor.reflection(port.A) > 70:
-        motor.run_for_degrees(port.D, -3, 100)
+        motor.run_for_degrees(port.D, -2, 100)
     else:
-        motor.run_for_degrees(port.F, 3, 100)
+        motor.run_for_degrees(port.F, 2, 100)
 motor.run_for_degrees(port.D, -280, 200)
 time.sleep(1.5)
 while not color_sensor.color(port.A) is color.BLUE:
@@ -31,17 +32,16 @@ motor.run_for_degrees(port.E, 30, 50)
 #raises the thrower a little bit higher so that gravity will pull it down and then the energy won't fall down
 time.sleep(0.9)
 #waits the amount of time it takes to move the thrower a bit higher
-motor.run_for_degrees(port.D, 200, 180)
+motor.run_for_degrees(port.D, 150, 180)
 time.sleep(1.3)
 #turns
-motor.run_for_degrees(port.D, 50, 200)
-motor.run_for_degrees(port.F, -50, 200)
-time.sleep(0.4)
+#Kool Distance sensor stuff.
+while not distance_sensor.distance(port.C)<= 15:
+    motor.run_for_degrees(port.D, 120, 200)
+    motor.run_for_degrees(port.F, -120, 200)
 #moves forward
-motor.run_for_degrees(port.D, 200, 200)
-motor.run_for_degrees(port.F, -200, 200)
-#waits the amount of time it takes to move backwards
 time.sleep(1)
+#waits the amount of time it takes to move backwards
 motor.run_for_degrees(port.E, 280, 250)
 #throws the energy
 time.sleep(0.7)
@@ -51,3 +51,5 @@ motor.run_for_degrees(port.E, -180, 300)
 time.sleep(1.50)
 #waits the amount of time it takes to move backwards
 motor.run_to_absolute_position(port.E, 10, 180)
+#36 excluding comments
+#19 comments
