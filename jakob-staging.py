4fb7331447e802a8901885937ff5ced1d5a3eff5
penguinboi2023 <153787407+penguinboi2023@users.noreply.github.com>
2024-03-06 18:36:22 -0800

    Update jakob-staging.py
    update code

--- jakob-staging.py
+++ jakob-staging.py
@@ -11,7 +11,26 @@ time.sleep(1)
 motor.run_to_absolute_position(port.E,12,100)
 time.sleep(1.8)
 ##insert movement towards energy here
-
+while not color_sensor.color(port.A) is color.RED:
+    if color_sensor.color(port.A) is color.BLACK:
+        motor.run_for_degrees(port.F,1,200)
+        time.sleep(0.001)
+    else:
+        motor.run_for_degrees(port.D,-1,200)
+        time.sleep(0.001)
+    if color_sensor.color(port.A) is color.RED:
+        motor.run_for_degrees(port.D,-170,200)
+        time.sleep(1)
+while not color_sensor.color(port.A) is color.BLUE:
+    if color_sensor.color(port.A) is color.BLACK:
+        motor.run_for_degrees(port.D,-1,200)
+        time.sleep(0.001)
+    else:
+        motor.run_for_degrees(port.F,1,200)
+        time.sleep(0.001)
+    if color_sensor.color(port.A) is color.BLUE:
+        motor.run_for_degrees(port.D,400,200)
+        motor.run_for_degrees(port.F,400,200)
 #aproaches energy
 motor.run_for_degrees(port.D,-400,200)
 motor.run_for_degrees(port.F,400,200)
@@ -20,25 +39,7 @@ time.sleep(0.9)
 motor.run_to_absolute_position(port.E,30,200)
 time.sleep(0.6)
 ##insert movement to mission here
-while not color_sensor.color(port.A) is color.RED:
-    if color_sensor.color(port.A) is color.BLACK:
-        motor.run_for_degrees(port.D,-1,200)
-        time.sleep(0.001)
-    else:
-        motor.run_for_degrees(port.F,1,200)
-        time.sleep(0.001)
-    if color_sensor.color(port.A) is color.RED:
-        motor.run_for_degrees(port.D,200,200)
-while not color_sensor.color(port.A) is color.BLUE:
-    if color_sensor.color(port.A) is color.BLACK:
-        motor.run_for_degrees(port.D,1,200)
-        time.sleep(0.001)      
-    else:
-        motor.run_for_degrees(port.F,-1,200)
-        time.sleep(0.001)
-    if color_sensor.color(port.A) is color.BLUE:
-        motor.run_for_degrees(port.D,200,200)
-        motor.run_for_degrees(port.F,200,200)
+
 #move backwards/approach mission
 motor.run_for_degrees(port.D,360,200)
 motor.run_for_degrees(port.F,-360,200)
