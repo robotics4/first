6df98cf4ba2cfb328ece017cd3af3d0e053cb3df
EthanYShen <153787534+EthanYShen@users.noreply.github.com>
2024-03-13 19:45:27 -0700

    Update ethanshen-staging.py

--- ethanshen-staging.py
+++ ethanshen-staging.py
@@ -29,7 +29,7 @@ motor.run_for_degrees(port.F, 700, 500)
 #move forward to grab the energy
 time.sleep(2.3)
 #waits the amount of time it takes to get to the energy
-motor.run_for_degrees(port.E, 30, 50)
+motor.run_to_absolute_position(port.E, 30, 50)
 #raises the thrower a little bit higher so that gravity will pull it down and then the energy won't fall down
 time.sleep(0.9)
 #waits the amount of time it takes to move the thrower a bit higher
@@ -37,10 +37,12 @@ motor.run_for_degrees(port.D, 150, 180)
 time.sleep(1.3)
 #turns
 #Kool Distance sensor stuff.
-while not distance_sensor.distance(port.C)<= 10:
-    motor.run_for_degrees(port.D, 10, 200)
-    motor.run_for_degrees(port.F, -10, 200)
+#while distance_sensor.distance(port.C) >= 50:
+    #motor.run_for_degrees(port.D, 10, 200)
+    #motor.run_for_degrees(port.F, -10, 200)
 #moves forward
+motor.run_for_degrees(port.D, 70, 200)
+motor.run_for_degrees(port.F, -70, 200)
 time.sleep(1)
 #waits the amount of time it takes to move backwards
 motor.run_for_degrees(port.E, 280, 250)
