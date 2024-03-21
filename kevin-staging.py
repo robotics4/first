402db2f3bf329e510ec158d725186a734344156b
Legosteve20 <153787330+Legosteve20@users.noreply.github.com>
2024-03-20 18:20:02 -0700

    Update kevin-staging.py

--- kevin-staging.py
+++ kevin-staging.py
@@ -3,31 +3,27 @@ import time
 import color
 import color_sensor
 from hub import port
-# imports all the things we need
 
+# imports all the things we need
+motor.run_to_absolute_position(port.E, 10, 60)
 while not color_sensor.color(port.A) is color.RED:
     if color_sensor.reflection(port.A) > 70:
-        motor.run_for_degrees(port.D, -3, 100)
+        motor.run_for_degrees(port.D, -1, 100)
     else:
-        motor.run_for_degrees(port.F, 3, 100)
+        motor.run_for_degrees(port.F, 1, 100)
 motor.run_for_degrees(port.D, -300, 200)
 # makes hub turn on red and follow the road
 time.sleep(2.2)
 while not color_sensor.color(port.A) is color.BLUE:
     if color_sensor.reflection(port.A) > 70:
-        motor.run_for_degrees(port.D, -3, 100)
+        motor.run_for_degrees(port.D, -1, 100)
     else:
-        motor.run_for_degrees(port.F, 3, 100)
+        motor.run_for_degrees(port.F, 1, 100)
         motor.run_to_absolute_position(port.E,10,60)
         # makes the hub turn on blue and follow the road
-time.sleep(0.3)
-motor.run_to_absolute_position(port.E,10,60)
-# sets hub motor E (arm) go to the level of the energy
 time.sleep(0.9)
-motor.run_for_degrees(port.D,-400,200)
-motor.run_for_degrees(port.F,400,200)
 # moves foward to get energy
-motor.run_to_absolute_position(port.E,30,50)
+motor.run_to_absolute_position(port.E,20,60)
 # makes the hand use gravity to hold energy
 time.sleep(0.9)
 motor.run_for_degrees(port.D,-50,200)
@@ -35,8 +31,17 @@ time.sleep(0.5)
 #turns
 motor.run_to_absolute_position(port.F,30,200)
 time.sleep(0.6)
-motor.run_for_degrees(port.D,-200,200)
-motor.run_for_degrees(port.F,200,200)
+motor.run_for_degrees(port.D,-400,200)
+motor.run_for_degrees(port.F,400,200)
 # move foward
 time.sleep(0.9)
-motor.run_to_absolute_position(port.E,180,80)
+# sets hub motor E (arm) go to the level of the energy
+time.sleep(0.9)
+motor.run_for_degrees(port.D,-400,200)
+motor.run_for_degrees(port.F,400,200)
+motor.run_to_absolute_position(port.E,80,80)
+time.sleep(1.5)
+motor.run_for_degrees(port.D,150,200)
+time.sleep(1.5)
+motor.run_for_degrees(port.D,150,200)
+motor.run_for_degrees(port.F,-150,200)
